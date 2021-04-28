b5576926474b45bf75ac7e73e1fedc3aaa66ab17
Tzvetelina25 <82510290+Tzvetelina25@users.noreply.github.com>
2021-04-28 13:24:10 +0300

    Add files via upload

---
+++ guess_the_number_without_zero.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter the interval: ";
+    cin>>a>>b;
+    srand(time(0)); //seed random number generator
+    int num = rand() % (b-a+1) + a; // random number between 1 and 100
+    cout << "Guess My Number Game"<<endl;
+
+    int guess = 0,counter=0;
+    while (guess != num) {
+        cout << "Enter a guess from the interval: ";
+        cin >> guess;
+
+        if(guess==0){
+            cout<< "0 is not an option. Game over."<<endl;
+            break;
+        }
+        if (guess > num) {
+            cout << "Too high!"<<endl;
+            counter++;
+        }
+        else if (guess < num)
+            {
+                cout << "Too low!"<<endl;
+                counter++;;
+            }
+        else {
+            counter++;
+            cout << "Correct! You got it in with "<<counter<<" attempts"<<endl;
+        }
+    }
+    return 0;
+}
