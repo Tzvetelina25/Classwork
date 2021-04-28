d3b85a06e1649335bb4c1c027e5d4f970c26a3da
Tzvetelina25 <82510290+Tzvetelina25@users.noreply.github.com>
2021-04-28 13:48:57 +0300

    Add files via upload

---
+++ leap_year.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <windows.h>
+using namespace std;
+
+int main ()
+{
+    int year;
+    cout<<"Enter a number:"<<endl;
+    cin>>year;
+    if(((year % 4 == 0) && (year % 100 !=0)) || year % 400 == 0){
+        cout<<year<<" is a leap year."<<endl;
+    }
+    else{
+        cout<<year<<" is not a leap year."<<endl;
+    }
+    return 0;
+}
+
